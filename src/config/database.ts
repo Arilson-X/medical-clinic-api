db0acba94b2f3eee48ef5862e25c87d92d7b7daa
Arilson-X <arilsonsouzacosta0@gmail.com>
2025-06-08 21:44:19 -0300

    Fix: updated read env file

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,14 +3,17 @@ import { Patient } from "../entities/Patient"
 import { Doctor } from "../entities/Doctor"
 import { Appointment } from "../entities/Appointment"
 import { User } from "../entities/User"
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: Number.parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME || "u_teste_api",
-  password: process.env.DB_PASSWORD || "connect123!",
-  database: process.env.DB_NAME || "medical_clinic",
+  host: process.env.DB_HOST,
+  port: Number.parseInt(process.env.DB_PORT!),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: process.env.NODE_ENV !== "production",
   logging: process.env.NODE_ENV === "development",
   entities: [Patient, Doctor, Appointment, User],
